64103aa40337d048189a4599cb8b97a1a0082ea6
micjez <michaljezynski@gmail.com>
2026-01-27 12:51:39 +0100

    initial commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,103 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Mind Within',
+  tagline: 'Explorations of mind, time, language, and illusion',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  //organizationName: 'swiadomosc', // Usually your GitHub org/user name.
+  //projectName: 'landscape', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: false,
+        blog: {
+          routeBasePath: '/',
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Mind Within',
+      logo: {
+        alt: 'Mind Within logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {to: '/about', label: 'About', position: 'left'},
+        {
+          href: 'mailto:hello@michaljezynski.com',
+          label: 'Contact',
+          position: 'left',
+          target: '_self',
+          className: 'navbar__link--no-external-icon',
+        },
+        {
+          to: '/atom.xml',
+          label: 'Atom',
+          position: 'right',
+          className: 'navbar__icon-link navbar__icon-link--rss',
+          'aria-label': 'RSS feed',
+        },
+      ],
+    },
+    footer: {
+      style: 'light',
+      copyright: `© ${new Date().getFullYear()} Mind Within`
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
